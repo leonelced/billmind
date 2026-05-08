13885c58c20cd17f82f0878c4a7bf18c3c505a2c
LeonelCedillo <leonel.cs.979@gmail.com>
2026-05-07 17:23:43 -0700

    feat: wire up express server with auth router

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import { errorMiddleWare } from "./api/middleware.js";
+import authRouter from "./api/routes.js"
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // Built-in middleware to parse JSON request bodies
 app.use(express.json());
+
+// Route all /api/auth/* requests to authRouter
+app.use("/api/auth", authRouter);
+
+// Error handling middleware in non-async code
+app.use(errorMiddleWare);
 
 async function main() {
   app.listen(PORT, () => {
