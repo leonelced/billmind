20343b6793378fbcf5befa6d72fedc6836bdaa4f
LeonelCedillo <leonel.cs.979@gmail.com>
2026-06-01 20:33:42 -0700

    style: add more shadcn components and apply some to Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Button } from "./ui/button";
+import { Button } from "#components/ui/button";
 
 
 export default function Navigation() {
   const navigate = useNavigate();
   const location = useLocation();
   const hideDashboardLink = ["/dashboard"].includes(location.pathname);
+  const hideNewBillButton = ["/bills/new"].includes(location.pathname);
 
   function handleLogout() {
     localStorage.clear();
@@ -15,9 +16,15 @@ export default function Navigation() {
   
   return (
     <div>
-      {!hideDashboardLink && <Link to="/dashboard">Dashboard</Link>}
-      <Link to="bills/new">+ New Bill</Link>
-      <Button onClick={handleLogout}>Logout</Button>
+      <Button variant="ghost">
+        {!hideDashboardLink && <Link to="/dashboard">Dashboard</Link>}
+      </Button>
+      <Button variant="ghost">
+        {!hideNewBillButton && <Link to="bills/new">+ New Bill</Link>}
+      </Button>
+      <Button onClick={handleLogout} variant="ghost">
+        Logout
+      </Button>
     </div>
   );
 }
