32cb7efd0aaf581d119b7ba9cf3d60e968ea8978
LeonelCedillo <leonel.cs.979@gmail.com>
2026-06-10 14:58:02 -0700

    fix: add refresh token duration in config and time for testing

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,24 @@
-import { type JWTConfig } from "./types/index.js";
 import "dotenv/config";
 
 
+type JWTConfig = {
+  defaultDuration: number;
+  refreshDuration: number;
+  secret: string;
+  issuer: string;
+}
+
+
 export const config: JWTConfig = {
-  defaultDuration: 60 * 60, // 1 hour in seconds
+  defaultDuration: 60 ,// * 15 access token: 15 minutes
+  // refreshDuration: 60 * 60 * 24 * 60 * 1000, // refresh token: 60 days in milliseconds,
+  refreshDuration: 60 * 3 * 1000,
   secret: envOrThrow("JWT_SECRET"),
   issuer: "billmind"
 }
+// refreshDuration: ms added to Date.now() which returns the 
+// current date and time represented as a number in milliseconds
+
 
 function envOrThrow(key: string) {
   const value = process.env[key]
