a5d76e391bf92227dcb36c31323ad53f0fbf414d
LeonelCedillo <leonel.cs.979@gmail.com>
2026-07-07 14:50:35 -0700

    ref: apply useBills hook on Dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,28 +1,8 @@
-import { useState, useEffect } from "react";
-import { type Bill } from "../types.js";
-import { fetchBills } from "../utils/bills.js";
+import useBills from "#hooks/useBills";
 import { BillsGrid } from "#components/BillsGrid";
 
 
 export default function Dashboard() { 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-  const [bills, setBills] = useState<Bill[]>([]);
-
-  // this runs once when the component first renders
-  useEffect(() => {
-    async function loadBills() {
-      try {
-        const bills: Bill[] = await fetchBills();
-        setBills(bills);
-      } catch(err) {
-        setError(err instanceof Error ? err.message : "Something went wrong")
-      } finally {
-        setLoading(false);
-      }
-    }
-    loadBills();
-  }, []); // the [] means "only run once on mount"
-
+  const { bills, loading, error } = useBills();
   return <BillsGrid bills={bills} loading={loading} error={error} />; 
 }
